20adb0ea06299ddf3eee2aa4b727955a48b4ea0f
marshvin <vincentmarsh01@gmail.com>
2025-08-06 11:46:19 +0300

    Enhance .gitignore to include additional file types for Java, Maven, and IDEs, while organizing exclusions for logs, environment files, Docker, and test reports.

---
+++ backend/src/main/java/com/mortgage/mortgageportal/MortgageportalApplication.java
@@ -0,0 +1,13 @@
+package com.mortgage.mortgageportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageportalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mortgage/mortgageportal/MortgageportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mortgage.mortgageportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
